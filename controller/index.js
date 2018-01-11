eac139922fe846d0daf8669272c165c3e1ebb00d
felix <felix@recast.ai>
2018-01-11 19:12:17 +0100

    feat(webchat): solve reset conversations and styling

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,7 @@ const Mopidy = require('mopidy');
 const _ = require('lodash');
 const SpotifyWebApi = require('spotify-web-api-node');
 const { clientId, clientSecret } = require('../spotify_cred/cred.json');
+const exec = require('child_process').exec;
 
 const spotifyApi = new SpotifyWebApi({
   clientId,
@@ -9,77 +10,79 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
+spotifyApi
+  .clientCredentialsGrant()
   .then(data => {
     console.log('The access token expires in ' + data.body.expires_in);
     console.log('The access token is ' + data.body.access_token);
-    console.log('The refresh token is ' + data.body['refresh_token']);
 
     spotifyApi.setAccessToken(data.body.access_token);
   })
   .catch(err => console.log('Something went wrong when retrieving an access token', err));
 
 // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
-spotifyApi.refreshAccessToken()
-  .then(function(data) {
+spotifyApi
+  .refreshAccessToken()
+  .then(data => {
     console.log('The access token has been refreshed!');
 
     // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
+    spotifyApi.setAccessToken(data.body.access_token);
+  })
+  .catch(err => {
     console.log('Could not refresh access token', err);
   });
 
-
 const getUri = data => {
   let uri = _.get(data, 'body.tracks.items[0].uri');
-  if (!uri)
-   uri = _.get(data, 'body.tracks.items[0].uri');
+  if (!uri) uri = _.get(data, 'body.tracks.items[0].uri');
   if (!uri) throw new Error('Cannot fing uri of tracks');
   return uri;
 };
 
 const playMusic = (req, res) => {
-  console.log('inside play music')
-  console.log(req.body)
-  const exec = require('child_process').exec;
-  console.log('inside set timeout')
-  exec("/home/pi/alexa-pi/controller/speech.sh 'ok I will play you the song'")
- 
+  const album = _.get(req.body.nlp, 'entities.album[0].raw', null);
+  const song = _.get(req.body.nlp, 'entities.song[0].raw', null);
+  let musician = _.get(req.body.nlp, 'entities.musician[0].raw', null);
+  if (!musician) {
+    musician = _.get(req.body.nlp, 'entities.person[0].raw', null);
+  }
+  const tracksString = song ? `track:${song}` : '';
+  const artistString = musician ? `artist:${musician}` : '';
+  const albumString = album ? `artist:${album}` : '';
+  const searchString = `${artistString} ${tracksString} ${albumString}`;
+  const speechSong = tracksString ? `the song ${tracksString}` : '';
+  let voiceToSend;
+  if (artistString && tracksString) {
+    voiceToSend = `Ok I will put you ${speechSong} by ${artistString}`;
+  } else if (artistString && !tracksString) {
+    voiceToSend = `Ok I will put you ${artistString}`;
+  } else if (!artistString && tracksString) {
+    voiceToSend = `Ok I will put you ${tracksString}`;
+  }
+
+  exec(`/home/pi/alexa-pi/controller/speech.sh ${voiceToSend}`);
+
   setTimeout(() => {
+    const play = () =>
+      spotifyApi
+        .searchTracks(searchString)
+        .then(getUri)
+        .then(mopidy.library.lookup)
+        .then(playlist => playlist)
+        .then(mopidy.tracklist.add)
+        .then(musicTrack => musicTrack[0])
+        .then(mopidy.playback.play)
+        .catch(err => {
+          console.log('Something went wrong!', err);
+        });
 
-  const play = () => {
-    const album = _.get(req.body.nlp, 'entities.album[0].raw', null);
-    const song = _.get(req.body.nlp, 'entities.song[0].raw', null);
-    let musician = _.get(req.body.nlp, 'entities.musician[0].raw', null);
-    if (!musician) {
-      musician = _.get(req.body.nlp, 'entities.person[0].raw', null);
-    }
-    const tracksString = song ? `track:${song}` : '';
-    const artistString = musician ? `artist:${musician}` : '';
-    const albumString = album ? `artist:${album}` : '';
-    const searchString = `${artistString} ${tracksString} ${albumString}`;
-
-    return spotifyApi
-      .searchTracks(searchString)
-      .then(getUri)
-      .then(mopidy.library.lookup)
-      .then(playlist => playlist)
-      .then(mopidy.tracklist.add)
-      .then(musicTrack => musicTrack[0])
-      .then(mopidy.playback.play)
-      .catch(err => {
-        console.log('Something went wrong!', err);
-      });
-  };
-
-  const mopidy = new Mopidy({
-    webSocketUrl: 'ws://localhost:6680/mopidy/ws/',
-  });
-  mopidy.on('state:online', play);
-  res.send();
-}, 10000);
-
+    const mopidy = new Mopidy({
+      webSocketUrl: 'ws://localhost:6680/mopidy/ws/',
+    });
+    mopidy.on('state:online', play);
+    res.send();
+  }, 10000);
 };
 
 const musicStyle = (req, res) => {
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const recastai = require('recastai').default
 
-var build = new recastai.build('a9a139557223331345e7d917072c8ce0', 'en')
+const recastai = require('recastai').default;
+const exec = require('child_process').exec;
+const build = new recastai.build('a9a139557223331345e7d917072c8ce0', 'en');
 
 const { musicStyle, playMusic } = require('./controller/');
 
@@ -21,14 +22,71 @@ app.post('/errors', (req, res) => {
 app.post('/musicStyle', musicStyle);
 app.post('/playSong', playMusic);
 
-app.post('/getVoice', function(req, res) {
-   console.log('get voice')
-   console.log(req.body)
-   build.dialog({ type: 'text', content: req.body.content}, { conversationId: 'CONVERSATION_ID' })
-  .then(function(res) {
-    res.send()
-  })   
-})
+app.post('/hello', (req, res) => {
+  const answerArray = [
+    'Hello, how are you, nice to see you back. Which song do you want to play today?',
+    'Hello my friend. Tell me a song, and I will play it for you!',
+  ];
+  const randomAnswer = Math.floor(Math.random() * answerArray.length);
+  console.log(answerArray[randomAnswer]);
+  res.send();
+  // exec(`/home/pi/alexa-pi/controller/speech.sh ${randomAnswer}`);
+});
+
+app.post('/goodbye', (req, res) => {
+  const answerArray = [
+    'See you soon, I hope you are happy about my service',
+    'Goodbye, very nice talking to you',
+  ];
+  const randomAnswer = Math.floor(Math.random() * answerArray.length);
+  console.log(answerArray[randomAnswer]);
+  // exec(`/home/pi/alexa-pi/controller/speech.sh ${randomAnswer}`);
+  res.send();
+});
+
+app.post('/getVoice', (req, res) => {
+  console.log('get voice');
+  console.log(req.body);
+  build
+    .dialog(
+      { type: 'text', content: req.body.content },
+      { conversationId: 'CONVERSATION_ID' }
+    )
+    .then(res => {
+      res.send();
+    });
+});
+
+app.post('/help', (req, res) => {
+  const answerArray = [
+    'Ok I will help, can you tell me what type of music you like?',
+    'Please tell me a style of music you enjoy listening',
+  ];
+  const randomAnswer = Math.floor(Math.random() * answerArray.length);
+  console.log(answerArray[randomAnswer]);
+  // exec(`/home/pi/alexa-pi/controller/speech.sh ${randomAnswer}`);
+  res.send();
+});
+
+app.post('/understand', (req, res) => {
+  const answerArray = ['What?', "I don't understand. Can you repeat?"];
+  const randomAnswer = Math.floor(Math.random() * answerArray.length);
+  console.log(answerArray[randomAnswer]);
+  // exec(`/home/pi/alexa-pi/controller/speech.sh ${randomAnswer}`);
+  res.send();
+});
+
+app.post('joke', (req, res) => {
+  const answerArray = [
+    'Today a man knocked on my door and asked for a small donation towards the local swimming pool. I gave him a glass of water',
+    'Feeling pretty proud of myself. The Sesame Street puzzle I bought said 3-5 years, but I finished it in 18 months.',
+    `Life is all about perspective. The sinking of the Titanic was a miracle to the lobsters in the ship's kitchen.`,
+  ];
+  const randomAnswer = Math.floor(Math.random() * answerArray.length);
+  console.log(answerArray[randomAnswer]);
+  // exec(`/home/pi/alexa-pi/controller/speech.sh ${randomAnswer}`);
+  res.send();
+});
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
